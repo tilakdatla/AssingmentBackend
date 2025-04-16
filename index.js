45644c36fd7ab7463dee080aa3b96d061f61b4dc
Tilak Datla <146624524+tilakdatla@users.noreply.github.com>
2025-04-16 11:58:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://assingment-frontend-y4ep.vercel.app/',
   methods: ['GET', 'POST', 'DELETE'],
   credentials: true
 }));
